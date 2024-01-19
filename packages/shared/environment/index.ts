448f054204ef3b23371b125fba70c0a7d9598745
rodrigopluz <rodrigopluz@gmail.com>
2024-01-19 18:35:53 -0300

    initial commit

--- packages/shared/environment/index.ts
+++ packages/shared/environment/index.ts
@@ -2,7 +2,7 @@ export const Environment = {
   LINE_LIMIT: 10,
   SEARCH_INPUT: 'Search...',
   EMPTY_LISTING: 'No records found.',
-  URL_BASE: 'http://localhost:3333',
+  URL_BASE: 'http://cleanport.com.br:3333',
   LOCAL_STORAGE_KEY__ACCESS_TOKEN: 'APP_ACCESS_TOKEN',
   LOCAL_STORAGE_USER: 'APP_USER',
   PROFILE_ADM: 1,
